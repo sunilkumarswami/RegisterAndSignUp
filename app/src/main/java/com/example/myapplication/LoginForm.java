6b06fb9fe4f8690713a4a91f649dc8502fa57e77
sunilkumarswami <sunilswamimukesh@gmail.com>
2019-12-13 00:30:54 +0530

    First Commit

---
+++ app/src/main/java/com/example/myapplication/LoginForm.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
+        getSupportActionBar().setTitle("Login Form");
+    }
+        public void btn_SignUpForm (View view){
+            startActivity(new Intent(getApplicationContext(), SignUp_Form.class));
+        }
+    }
+
